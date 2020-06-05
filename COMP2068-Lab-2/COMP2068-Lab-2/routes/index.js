f1ed507141472a828bd8c7c7400693ddd02fc63e
Yhames1978 <54914229+Yhames1978@users.noreply.github.com>
2020-06-04 20:35:56 -0400

    completed code

--- COMP2068-Lab-2/COMP2068-Lab-2/routes/index.js
+++ COMP2068-Lab-2/COMP2068-Lab-2/routes/index.js
@@ -4,7 +4,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    var answer= "";
+
+    function calculate(method, x, y) {
+
+        var number1 = parseInt(x);
+        var number2 = parseInt(y);
+
+        if (method === "add") {
+            answer = "Addition: " + x + " + " + y + " = " + (number1 + number2);
+            console.log(answer);
+        }
+        else if (method === "subtract") {
+            answer = "Subtraction: " + x + " - " + y + " = " + (number1 - number2);
+            console.log(answer);
+        }
+        else if (method === "multiply") {
+            answer = "Multiplication: " + x + " * " + y + " = " + (number1 * number2);
+            console.log(answer);
+        }
+        else if (method === "divide") {
+            answer = "Division: " + x + " / " + y + " = " + (number1 / number2);
+            console.log(answer);
+        }
+        else {
+            answer = "Error";
+            console.log(answer);
+        }
+    }
+        if (Object.keys(req.query).length > 0) {
+            calculate(req.query.method, req.query.x, req.query.y);
+   }
+
+    
+    
+    res.render('index', { title: 'Operations in JavaScript', calculate: answer });
 });
+
 
 module.exports = router;
